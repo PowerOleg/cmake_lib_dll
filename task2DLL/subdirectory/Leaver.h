717b4b42472cb851d516ffeedc1efe3e69ca202b
PowerOleg <geraskin.oleg@mail.ru>
2024-03-31 19:51:34 +0300

    task2cmakeDLL.workWithoutMacros

--- task2DLL/subdirectory/Leaver.h
+++ task2DLL/subdirectory/Leaver.h
@@ -1,17 +1,11 @@
 #pragma once
 #include <iostream>
 
-#ifndef EXPORT_LIB
-	#define EXPORT_IMPORT_API __declspec(dllexport)
-#else
-	#define EXPORT_IMPORT_API __declspec(dllimport)
-#endif
-
 class Leaver
 {
 private:
 	char answer[256]{"До свидания, "};
 public:
-	EXPORT_IMPORT_API char* leave(char* name);
+	char* leave(char* name);
 };
 
