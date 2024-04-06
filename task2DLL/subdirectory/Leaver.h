66a449fb843a8dd12d6ef2ec23ba60db4d31cea6
PowerOleg <geraskin.oleg@mail.ru>
2024-04-06 15:43:44 +0300

    CMakeList target_compile_definitions()

--- task2DLL/subdirectory/Leaver.h
+++ task2DLL/subdirectory/Leaver.h
@@ -1,7 +1,13 @@
 #pragma once
 #include <iostream>
 
-class Leaver
+#ifdef LEAVER_EXPORTS
+#define LEAVER_API __declspec(dllexport)
+#else 
+#define LEAVER_API __declspec(dllimport)
+#endif
+
+class LEAVER_API Leaver
 {
 private:
 	char answer[256]{"До свидания, "};
